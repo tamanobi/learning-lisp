69ff3134ec989ab14bcaf041f14f74ae55197688
Yasu <yasu@pixiv.co.jp>
2017-11-07 03:37:31 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Learning Common Lisp(SBCL)
---
+++ test.lisp
@@ -0,0 +1,8 @@
+(defun greeting ()
+  (print "Welcome to the world of CommonLisp!"))
+
+(cons 1 3)
+(+ 1 (- 10 2))  ; the new line
+(in-package :vlime) ; the new line
+(symbol-value (find-symbol "*PACKAGE*" "COMMON-LISP"))  ; the new line
+(reverse '(test aa) )
